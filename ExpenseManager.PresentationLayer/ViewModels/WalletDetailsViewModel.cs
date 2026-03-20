07cc41ef093998554bad89120343a0b7fabbc425
Vlad Panko <vvvladpanko@gmail.com>
2026-03-20 02:55:31 +0200

    refactor: use RelayCommand

--- ExpenseManager.PresentationLayer/ViewModels/WalletDetailsViewModel.cs
+++ ExpenseManager.PresentationLayer/ViewModels/WalletDetailsViewModel.cs
@@ -1,10 +1,9 @@
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using ExpenseManager.BusinessLayer.Dtos;
 using ExpenseManager.BusinessLayer.Interfaces;
 using ExpenseManager.PresentationLayer.Pages;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ExpenseManager.PresentationLayer.ViewModels;
 
@@ -13,10 +12,10 @@ public partial class WalletDetailsViewModel : ObservableObject, IQueryAttributab
     private readonly IWalletService _walletService;
     private readonly ITransactionService _transactionService;
 
+    [ObservableProperty]
     private WalletResponse _currentWallet;
     [ObservableProperty]
     private ObservableCollection<TransactionResponse> _transactions;
-    private TransactionResponse _selectedTransaction;
     [ObservableProperty]
     private string _walletTotalText;
 
@@ -26,46 +25,28 @@ public partial class WalletDetailsViewModel : ObservableObject, IQueryAttributab
         _transactionService = transactionService ?? throw new ArgumentNullException(nameof(transactionService));
     }
     
-    public WalletResponse CurrentWallet
+    [RelayCommand]
+    private void LoadTransaction(Guid transactionGuid)
     {
-        get => _currentWallet;
-        private set
-        {
-            _currentWallet = value;
-
-            var list = _transactionService.GetAllByWallet(
-                new GetTransactionsByWalletRequest(_currentWallet.Guid)
-            ).ToList();
-            Transactions = new ObservableCollection<TransactionResponse>(list);
-
-            decimal total = _walletService.GetTotal(new GetWalletTotalRequest(_currentWallet.Guid));
-            WalletTotalText = $"Total expenses and incomes: {total} {_currentWallet.Currency}";
-            
-            OnPropertyChanged();
-        }
-    }
-    
-    public TransactionResponse SelectedTransaction
-    {
-        get => _selectedTransaction;
-        set
-        {
-            _selectedTransaction = value;
-            if (value is not null)
+        Shell.Current.GoToAsync($"{nameof(TransactionDetailsPage)}",
+            new Dictionary<string, object>
             {
-                Shell.Current.GoToAsync($"{nameof(TransactionDetailsPage)}",
-                    new Dictionary<string, object>
-                    {
-                        { "TransactionGuid", value.Guid },
-                        { "WalletGuid", CurrentWallet.Guid }
-                    });
-            }
-        }
+                { "TransactionGuid", transactionGuid },
+                { "WalletGuid", CurrentWallet.Guid }
+            });
     }
     
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         Guid walletGuid = (Guid)query["WalletGuid"];
         CurrentWallet = _walletService.GetByGuid(new GetWalletRequest(walletGuid));
+        
+        var list = _transactionService.GetAllByWallet(
+            new GetTransactionsByWalletRequest(walletGuid)
+        ).ToList();
+        Transactions = new ObservableCollection<TransactionResponse>(list);
+
+        decimal total = _walletService.GetTotal(new GetWalletTotalRequest(walletGuid));
+        WalletTotalText = $"Total expenses and incomes: {total} {CurrentWallet.Currency}";
     }
 }
--- ExpenseManager.PresentationLayer/ViewModels/WalletsViewModel.cs
+++ ExpenseManager.PresentationLayer/ViewModels/WalletsViewModel.cs
@@ -1,38 +1,27 @@
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.Input;
 using ExpenseManager.BusinessLayer.Dtos;
 using ExpenseManager.BusinessLayer.Interfaces;
 using ExpenseManager.PresentationLayer.Pages;
 
 namespace ExpenseManager.PresentationLayer.ViewModels;
 
-public class WalletsViewModel
+public partial class WalletsViewModel
 {
     private readonly IWalletService _walletService;
-    
-    private WalletResponse _selectedWallet;
     
     public ObservableCollection<WalletResponse> Wallets { get; set; }
 
     public WalletsViewModel(IWalletService walletService)
     {
         _walletService = walletService ?? throw new ArgumentNullException(nameof(walletService));
-        
         Wallets =  new ObservableCollection<WalletResponse>(_walletService.GetAll());
     }
     
-    public WalletResponse SelectedWallet
+    [RelayCommand]
+    private void LoadWallet(Guid walletGuid)
     {
-        get => _selectedWallet;
-        set
-        {
-            _selectedWallet = value;
-            if (value is not null)
-            {
-                Shell.Current.GoToAsync($"{nameof(WalletDetailsPage)}",
-                    new Dictionary<string, object> { { "WalletGuid", value.Guid } });
-            }
-        }
+        Shell.Current.GoToAsync($"{nameof(WalletDetailsPage)}",
+            new Dictionary<string, object> { { "WalletGuid", walletGuid } });
     }
 }
